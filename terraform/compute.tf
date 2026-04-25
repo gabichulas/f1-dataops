98f1c2433448dd8ff21f91455f43d78bc2217ccb
gabichulas <gabylopezromero13@gmail.com>
2026-04-25 20:31:58 -0300

    Stop hardcoding metrics-type-prefixes

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -114,6 +114,7 @@ resource "google_compute_instance" "obs_vm" {
     gsutil cp gs://f1-dataops-f1-pipeline-configs/prometheus.yaml /opt/observability/prometheus/prometheus.yaml
     gsutil cp gs://f1-dataops-f1-pipeline-configs/local-config.yaml /opt/observability/loki/local-config.yaml
     gsutil cp gs://f1-dataops-f1-pipeline-configs/docker-compose.yaml /opt/observability/docker-compose.yaml
+    gsutil cp gs://f1-dataops-f1-pipeline-configs/.env /opt/observability/.env
 
     cd /opt/observability
     docker compose up -d
